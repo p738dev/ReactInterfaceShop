6e5b90b54f6bd00cf99b39873f984fa2a303f6a5
p738dev <pmech14@interia.pl>
2024-05-22 08:52:03 +0200

    Basic settings files and need images

---
+++ src/utils/images.js
@@ -0,0 +1,5 @@
+import loader from "../assets/images/loader.svg";
+import correct from "../assets/images/correct.png";
+import shoppingCart from "../assets/images/shopping_cart.png";
+
+export { loader, correct, shoppingCart };
